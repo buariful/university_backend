b719ff079e82e3adcfdf636a326080a02f54a8a7
ariful <arifulislam321054@gmail.com>
2024-09-04 17:57:26 +0600

    catchAsync fn added

--- src/modules/student/student.controller.ts
+++ src/modules/student/student.controller.ts
@@ -1,45 +1,23 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { StudentServices } from './student.service';
 
-const createStudent = async (req: Request, res: Response) => {
-  try {
-    const { student } = req.body;
-    const result = await StudentServices.createStudentIntoDB(student);
-
-    res.status(201).json({
-      success: true,
-      message: 'Student is created successfully',
-      data: result,
-    });
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(error);
-    res.status(400).json({
-      success: false,
-      message: error,
-    });
-  }
-};
-const getStudents = async (req: Request, res: Response) => {
-  try {
-    const result = await StudentServices.getAllStudents();
-
-    res.status(200).json({
-      success: true,
-      message: 'All students',
-      data: result,
-    });
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(error);
-    res.status(400).json({
-      success: false,
-      message: error,
-    });
-  }
+const catchAsync = (fn: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  };
 };
 
-const getSingleStudent = async (req: Request, res: Response) => {
+const getStudents = catchAsync(async (req, res) => {
+  const result = await StudentServices.getAllStudents();
+
+  res.status(200).json({
+    success: true,
+    message: 'All students',
+    data: result,
+  });
+});
+
+const getSingleStudent: RequestHandler = async (req, res) => {
   try {
     const { studentId } = req.params;
     const result = await StudentServices.getSingleStudent(studentId);
@@ -60,7 +38,6 @@ const getSingleStudent = async (req: Request, res: Response) => {
 };
 
 export const StudentControllers = {
-  createStudent,
   getStudents,
   getSingleStudent,
 };
--- src/modules/student/student.route.ts
+++ src/modules/student/student.route.ts
@@ -3,7 +3,6 @@ import { StudentControllers } from './student.controller';
 const router = express.Router();
 
 router.get('/', StudentControllers.getStudents);
-// router.post('/create', StudentControllers.createStudent);
 router.get('/:studentId', StudentControllers.getSingleStudent);
 
 export const StudentRoutes = router;
