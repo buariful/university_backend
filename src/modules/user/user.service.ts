99049855d173e3b71002816a9506c3724038d7a9
ariful <arifulislam321054@gmail.com>
2024-09-15 16:53:47 +0600

    student create fn updated

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -8,6 +8,12 @@ import { User } from './user.model';
 import { generateStudentId } from './user.utils';
 
 const createStuentIntoDB = async (password: string, payload: TStudent) => {
+  // checking if student exists with same email
+  const findStudentWithSameEmail = await Student.find({ email: payload.email });
+  if (findStudentWithSameEmail?.length) {
+    throw new Error('Student with this email already exists');
+  }
+
   // create a user object
   const userData: Partial<TUser> = {};
   // if password is not given, use default password.
@@ -19,11 +25,14 @@ const createStuentIntoDB = async (password: string, payload: TStudent) => {
   const admissionSemester = await AcademicSemester.findById(
     payload.admissionSemester,
   );
+  if (!admissionSemester) {
+    throw new Error('Admission semester not found');
+  }
 
   //  set manually generated id
   userData.id = await generateStudentId(admissionSemester);
 
-  //  create an user
+  //  create a user
   const newUser = await User.create(userData);
 
   // create a stuent
--- src/modules/user/user.utils.ts
+++ src/modules/user/user.utils.ts
@@ -16,12 +16,28 @@ export const findLastStudentId = async () => {
     })
     .lean();
 
-  return lastStudent?.id ? lastStudent?.id?.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent?.id : undefined;
 };
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
   // first time 0000
-  const currentId = ((await findLastStudentId()) || 0).toString();
+  let currentId = (0).toString();
+  const lastStudentId = await findLastStudentId(); // 2030 01 0002
+  const lasStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lasStudentYear = lastStudentId?.substring(0, 4);
+
+  const currentSemesterCode = payload?.code;
+  const currentSemesterYear = payload?.year;
+
+  if (
+    lastStudentId &&
+    lasStudentSemesterCode === currentSemesterCode &&
+    lasStudentYear === currentSemesterYear
+  ) {
+    currentId = lastStudentId?.substring(6);
+  }
+
+  // const currentId = ((await findLastStudentId()) || 0).toString();
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payload.year}${payload.code}${incrementId}`;
 
