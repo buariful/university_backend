b67beb3456e648e921dcf1c4b0364f3a7a028fad
ariful <arifulislam321054@gmail.com>
2024-09-20 16:03:32 +0600

    academic department updated

--- src/modules/academicDepartment/academicDepartment.model.ts
+++ src/modules/academicDepartment/academicDepartment.model.ts
@@ -18,6 +18,30 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 );
 
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExists = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+
+  if (isDepartmentExists) {
+    throw new Error('This department is already exists');
+  }
+
+  next();
+});
+
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  // findOneAndUpdate e j query thiye find korbe seta ekhane get hobe, example: {_id: 1w6er5we9r5c3g4w9er65};
+  const query = this.getQuery();
+  const isDepartmentExists = await AcademicDepartment.findOne(query);
+
+  if (!isDepartmentExists) {
+    throw new Error("This department doesn't exists");
+  }
+
+  next();
+});
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema,
